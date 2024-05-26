8144d3f901c59c737f37745bb4c3d19c1a79a3df
Happy Patel <140176910+happy1511@users.noreply.github.com>
2024-05-26 06:26:29 +0000

    build cahnges

--- Game/settings.py
+++ Game/settings.py
@@ -64,7 +64,12 @@ WSGI_APPLICATION = 'Game.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
